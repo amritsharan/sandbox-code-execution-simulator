3703325ff4cea603da5eef5dbb14b6a79e170854
AMRIT SR <amritsr2005@gmail.com>
2026-01-29 08:36:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-409394776-3ac19",
+  "appId": "1:229753485958:web:3f89fc2b7cfeb184582f07",
+  "apiKey": "AIzaSyDpnhwjLsI-YvBv3NLuLMOp5B1pmUBRtMI",
+  "authDomain": "studio-409394776-3ac19.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "229753485958"
+};
